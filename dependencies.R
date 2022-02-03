21cab923ae08807db7d374189c16e7fd498859e6
Shivani Srivastava <shivani141296@gmail.com>
2022-02-03 13:15:34 +0530

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,4 +1,4 @@
 # Install / Load relevant packages
 if(!"pacman" %in% installed.packages()[,"Package"]) install.packages("pacman")
-pacman::p_load(shiny,tidyverse, tidytext, stringr, Matrix,DT,data.table)
+pacman::p_load(shiny,tidyverse, tidytext, stringr, Matrix,DT,data.table, dplyr)
 #pacman::p_load(DT)
